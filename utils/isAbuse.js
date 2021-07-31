518db55175d595840521e0009c0ddebd8a06a49d
Camrado <blackandwhite.b17w@gmail.com>
2021-07-31 20:47:03 +0400

    Code refactor

--- utils/isAbuse.js
+++ utils/isAbuse.js
@@ -4,17 +4,8 @@ const decentWords = require('../decent-words.json')["decent-words"];
 const isAbuse = (text) => {
   if (typeof text !== 'string') return; // check that it's a text message not sticker or anything else
 
-  let isAbuse = false;
   text = removeDuplicate(text.toLowerCase());
-
-  for (let i = 0; i < decentWords.length; i++) {
-    if (text.includes(decentWords[i])) {
-      isAbuse = true;
-      break;
-    }
-  }
-
-  return isAbuse;
+  return decentWords.some((word) => text.includes(word)); // if text contains "decent" word, return true, else false
 };
 
 module.exports = isAbuse;
