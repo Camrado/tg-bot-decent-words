c671ce4dc38aa74580fca4e1f3cf74592f90629d
Camrado <blackandwhite.b17w@gmail.com>
2021-08-01 13:58:20 +0400

    Reset. Deleted everything related to pm2

--- utils/isAbuse.js
+++ utils/isAbuse.js
@@ -2,7 +2,7 @@ const removeDuplicate = require('./removeDuplicate');
 const decentWords = require('../decent-words.json')["decent-words"];
 
 const isAbuse = (text) => {
-  // if (typeof text !== 'string') return; // check that it's a text message not sticker or anything else
+  if (typeof text !== 'string') return; // check that it's a text message not sticker or anything else
 
   text = removeDuplicate(text.toLowerCase());
   return decentWords.some((word) => text.includes(word));
